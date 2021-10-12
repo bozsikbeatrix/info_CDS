673d40c6e285e4b4baacc6c411377b3f4d4a76fb
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2021-10-12 07:34:11 +0300

    11 es modszer

--- 2021_10_12_lab_4/main.cpp
+++ 2021_10_12_lab_4/main.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 void divPrim(int n, int &s){
     s=0;
-int nr=0, oszto=2;
-while(oszto<=n){
+
+}
+
+
+int main()
+{
+    int s, n;
+    s=0;
+    cout << "n=";
+    cin >> n;
+    int nr=0, oszto=2;
+    while(oszto<=n){
     nr=0;
     while (n%oszto==0){
         n/=oszto;
@@ -15,15 +25,7 @@ while(oszto<=n){
         s+=oszto;
     }
     oszto++;
-}
-}
-
-int main()
-{
-    int s;
-    divPrim(360, s);
-    cout << s << endl;
-    divPrim(16, s);
-    cout << s << endl;
+    }
+    cout << s;
     return 0;
 }
